630cf15538839f232eab54b360c078efbe4e271c
heoh <h0h6h2h5@gmail.com>
2021-12-23 21:04:06 +0000

    sadf

--- dist/index.js
+++ dist/index.js
@@ -8442,21 +8442,61 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
+const SECONDS_TO_MILLISECONDS = 1000;
+const REMIND_MARKER = '<span id="review-reminder-remind" />';
+
 const core = __nccwpck_require__(2810);
 const github = __nccwpck_require__(4176);
+const owner = github.context.payload.sender && github.context.payload.sender.login;
+const repo = github.context.payload.repository && github.context.payload.repository.name;
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const token = core.getInput('token', { required: true });
+    const expirationTime = core.getInput('expiration-time', { required: true });
+    const message = core.getInput('message');
+
+    const octokit = github.getOctokit(token);
+    const expirationDate = new Date(Date.now() - (expirationTime * SECONDS_TO_MILLISECONDS));
+
+    const pulls = await getPullRequests(octokit);
+    pulls.forEach(async (pull) => {
+      if (await needToRemind(octokit, pull, expirationDate)) {
+        await remind(octokit, pull, message);
+      }
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+async function getPullRequests(octokit) {
+  const { data } = await octokit.rest.pulls.list({ owner, repo, state: 'open' });
+  return data;
+}
+
+async function needToRemind(octokit, pull, expirationDate) {
+  const createdDate = new Date(pull.created_at);
+  const expired = createdDate <= expirationDate;
+  if (!expired) {
+    return false;
+  }
+
+  const { data } = await octokit.rest.issues.listComments({ owner, repo, issue_number: pull.number });
+  const alreadyRemind = data.some(comment => comment.body.startsWith(REMIND_MARKER));
+  return !alreadyRemind;
+}
+
+async function remind(octokit, pull, message) {
+  await octokit.rest.issues.createComment({
+    owner,
+    repo,
+    issue_number: pull.number,
+    body: `${REMIND_MARKER}${message}`,
+  });
+}
+
+run();
 
 })();
 
