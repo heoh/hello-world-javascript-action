3107c5e34e3e1217f6dafca406657923f88ab6ea
heoh <h0h6h2h5@gmail.com>
2021-12-23 19:52:36 +0000

    asd

--- index.js
+++ index.js
@@ -9,52 +9,18 @@ const repo = github.context.payload.repository && github.context.payload.reposit
 async function run() {
   try {
     const token = core.getInput('token', { required: true });
-    const expiryPeriod = 60 * SECONDS_TO_MILLISECONDS;
-    const message = "It's time to end this pull request!";
+    const expirationTime = core.getInput('expiration-time', { required: true });
+    const message = core.getInput('message', { required: true });
 
     const octokit = github.getOctokit(token);
-    const expiryDate = new Date(Date.now() - expiryPeriod);
+    const expirationDate = new Date(Date.now() - (expirationTime * SECONDS_TO_MILLISECONDS));
 
     const pulls = await getPullRequests(octokit);
-    // const owner = github.context.payload.sender && github.context.payload.sender.login;
-    // const repo = github.context.payload.repository && github.context.payload.repository.name;
-    // const { data } = await octokit.rest.pulls.list({ owner, repo, state: 'open' });
-    // const pulls = data;
-
     pulls.forEach(async (pull) => {
-      if (await needToRemind(octokit, pull, expiryDate)) {
-      // const createdDate = new Date(pull.created_at);
-      // const { data: aa } = await octokit.rest.issues.listComments({ owner, repo, issue_number: pull.number });
-      // const needToRemind = (createdDate <= expiryDate) && aa.some(comment => comment.body?.startsWith(REMIND_MARKER));
-      // if (needToRemind) {
+      if (await needToRemind(octokit, pull, expirationDate)) {
         await remind(octokit, pull, message);
       }
     });
-
-
-    // const lastExpiryDate = new Date(Date.now() - expiryPeriod);
-    // const isExpired = pr => (new Date(pr.created_at)) <= lastExpiryDate;
-
-    // for (const pr of data) {
-    //   if (!isExpired(pr) || !) {
-    //     continue;
-    //   }
-
-    //   const createdDate = new Date(pr.created_at);
-    //   const expiryDate = new Date(Date.parse(createdDate) + expiryPeriod);
-    //   if (now >= expiryDate) {
-
-    //   }
-    // }
-
-    // console.log(data);
-
-    // console.log(`Hello ${token}!`);
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -65,9 +31,9 @@ async function getPullRequests(octokit) {
   return data;
 }
 
-async function needToRemind(octokit, pull, expiryDate) {
+async function needToRemind(octokit, pull, expirationDate) {
   const createdDate = new Date(pull.created_at);
-  const expired = createdDate <= expiryDate;
+  const expired = createdDate <= expirationDate;
   if (!expired) {
     return false;
   }
