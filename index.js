779787d13ee580201fbb85e908c56892b4d07fd7
heoh <h0h6h2h5@gmail.com>
2021-12-23 14:19:47 +0000

    asd

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const token = core.getInput('token', { required: true });
-  const octokit = github.getOctokit(token);
-  const owner = github.context.payload.sender && github.context.payload.sender.login;
-  const repo = github.context.payload.repository && github.context.payload.repository.name;
-  const { data } = await octokit.pulls.list({ owner, repo, state: 'open' });
-
-  console.log(data);
-
-  console.log(`Hello ${token}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const token = core.getInput('token', { required: true });
+    const octokit = github.getOctokit(token);
+    const owner = github.context.payload.sender && github.context.payload.sender.login;
+    const repo = github.context.payload.repository && github.context.payload.repository.name;
+    const { data } = await octokit.pulls.list({ owner, repo, state: 'open' });
+  
+    console.log(data);
+  
+    console.log(`Hello ${token}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }  
 }
+
+run();
